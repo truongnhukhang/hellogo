bbca91e2c368187d3d3ae985beb86140305d37b6
sicacho <trustme013@gmail.com>
2019-06-11 23:15:13 +0700

    Update heapsort

--- sorting/HeapSort.go
+++ sorting/HeapSort.go
@@ -11,33 +11,51 @@ func main() {
 	//for i:=0;i<len(a);i++ {
 	//	fmt.Print(strconv.Itoa(a[i]) + " ");
 	//}
-	var length = len(a)
-	for i := 1; i <= len(a); i++ {
-		maxHeapBuild(a, i)
-		swapValue(a, 0, length-1)
-		length--
-	}
+	sortInplace(a)
 	for i := 0; i < len(a); i++ {
 		fmt.Print(strconv.Itoa(a[i]) + " ")
 	}
 }
 
-func maxHeapBuild(a []int, index int) {
+func sortInplace(a []int) {
+	toMaxHeap(a)
+	var length = len(a)
+	for i := length; i > 1; i-- {
+		swapValue(a, 0, i-1)
+		length--
+		maxHeapBuild(a, 1, length)
+	}
+}
+
+func sort(a []int) []int {
+	var length = len(a)
+	var result []int
+	for length != 0 {
+		maxHeapBuild(a, 1, len(a))
+		result = append(result, a[0])
+		a = a[1:len(a)]
+		length--
+	}
+	return result
+}
+func toMaxHeap(a []int) {
+	for i := len(a) / 2; i > 0; i-- {
+		maxHeapBuild(a, i, len(a))
+	}
+}
+func maxHeapBuild(a []int, index int, length int) {
 	var left = 2 * index
 	var right = 2*index + 1
 	var largest = index
-	if left < len(a) && a[index-1] < a[left-1] {
+	if left <= length && a[index-1] < a[left-1] {
 		largest = left
-		swapValue(a, index-1, left-1)
 	}
-	if right < len(a) && a[index-1] < a[right-1] {
-		if largest == index {
-			largest = right
-		}
-		swapValue(a, index-1, right-1)
+	if right <= length && a[largest-1] < a[right-1] {
+		largest = right
 	}
 	if largest != index {
-		maxHeapBuild(a, largest)
+		swapValue(a, index-1, largest-1)
+		maxHeapBuild(a, largest, length)
 	}
 }
 
