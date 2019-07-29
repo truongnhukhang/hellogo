46325e31ffeb08efe180877d975406f2153e5878
sicacho <trustme013@gmail.com>
2019-07-30 06:50:29 +0700

    MinPartitionArray

--- array/MinPartitionArray.go
+++ array/MinPartitionArray.go
@@ -1,1 +1,18 @@
 package main
+
+import "math"
+
+func main() {
+	s := []float64{10, 20, 15, 5, 25}
+	println(int(minPartition(s, len(s)-1, 0, 0)))
+}
+
+func minPartition(s []float64, index int, sum1 float64, sum2 float64) float64 {
+	if index < 0 {
+		return math.Abs(sum1 - sum2)
+	}
+
+	include := minPartition(s, index-1, sum1+s[index], sum2)
+	exclude := minPartition(s, index-1, sum1, sum2+s[index])
+	return math.Min(include, exclude)
+}
