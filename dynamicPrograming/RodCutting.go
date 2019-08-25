43611a2dac878eeedbc9daa7f171b6dceb3b03cc
sicacho <trustme013@gmail.com>
2019-08-25 20:28:20 +0700

    keep learning

---
+++ dynamicPrograming/RodCutting.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	price := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	price[0] = 1
+	price[1] = 5
+	price[2] = 8
+	price[3] = 9
+	price[4] = 10
+	price[5] = 17
+	price[6] = 17
+	price[7] = 20
+	price[8] = 24
+	price[9] = 30
+	fmt.Println(findMaxProfit(4, price))
+}
+
+func findMaxProfit(rodLength int, price []int) int {
+	if rodLength < 0 {
+		return 0
+	}
+	max := price[rodLength-1]
+	for i := 1; i < rodLength; i++ {
+		tempMax := price[i-1] + findMaxProfit(rodLength-i, price)
+		max = int(math.Max(float64(max), float64(tempMax)))
+	}
+	return max
+}
