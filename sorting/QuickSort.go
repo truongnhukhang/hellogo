d6493a5f96d12c505d18989014de3b378eb5a9f6
sicacho <trustme013@gmail.com>
2019-11-17 23:14:35 +0700

    update QuickSort

--- sorting/QuickSort.go
+++ sorting/QuickSort.go
@@ -1,36 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	a := []int{8, 1, 9, 2, 4, 5, 4, 2, 3, 10, 6, 7}
 	quickSort(a, 0, len(a)-1)
 	for i := range a {
-		fmt.Print(a[i])
+		fmt.Print(" " + strconv.Itoa(a[i]))
 	}
 }
 
 func quickSort(a []int, start int, end int) {
-	if start != end {
-		pivot := partition(a, start, end)
-		quickSort(a, start, pivot)
-		quickSort(a, pivot+1, end)
+	if start < end {
+		p := partition(a, start, end)
+		quickSort(a, start, p-1)
+		quickSort(a, p+1, end)
 	}
 }
 
 func partition(a []int, start int, end int) int {
-	pivotValue := a[end]
+	pivot := end
+	i := start
 	flag := start
-	for j := start; j < end; j++ {
-		if a[j] < pivotValue {
-			a[flag], a[j] = a[j], a[flag]
+	for ; i < end; i++ {
+		if a[i] <= a[pivot] {
+			a[flag], a[i] = a[i], a[flag]
 			flag++
 		}
 	}
-	if flag != end {
-		a[flag], a[end] = a[end], a[flag]
-		return flag
-	} else {
-		return flag - 1
-	}
+	a[flag], a[pivot] = a[pivot], a[flag]
+	return flag
 }
